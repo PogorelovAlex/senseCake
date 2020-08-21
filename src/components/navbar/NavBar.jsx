a917c6bdee72919ff01b8685e8bf01880c965a54
Alexei Pogorelov <alexeipogorelovbn@yandex.ru>
2020-08-21 17:40:01 +0300

    MenuButton

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,14 +1,18 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
-import Container from "@material-ui/core/Container"
+import IconButton from "@material-ui/core/IconButton"
+import MenuOutlinedIcon from "@material-ui/icons/MenuOutlined"
+import Hidden from "@material-ui/core/Hidden"
+
 
 import Categories from "../navbar/categories/Categories"
 import Logo from "../navbar/logo/Logo"
-import "./NavBar.scss"
 import Menu from "./menu/Menu"
 
-// import'../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./NavBar.scss"
+
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,11 +28,26 @@ function NavBar() {
         <Logo />
         <Menu />
       </Grid>
-      <Hidden smDown>
-        <Grid item xs={9} justify="flex-end">
-          <Categories />
-        </Grid>
+      
+        <Grid item xs={9} 
+        direction="row"
+        justify="flex-end"
+        alignItems="baseline">
+         <Hidden smDown> <Categories />
+         </Hidden>
+        <Hidden mdUp>
+          <Grid item xs={12} 
+          container
+         direction="row-reverse"
+         justify="flex-start"
+         alignItems="flex-end">
+           
+           <IconButton aria-label="MenuOutlined">
+        <MenuOutlinedIcon />
+      </IconButton> </Grid>
       </Hidden>
+       </Grid>
+      
     </Grid>
   )
 }
