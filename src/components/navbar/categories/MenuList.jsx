4cb6e04a6d5719aedebf85a2fac867ed355f09da
Alexei Pogorelov <alexeipogorelovbn@yandex.ru>
2020-08-27 19:04:15 +0300

    work

--- src/components/navbar/categories/MenuList.jsx
+++ src/components/navbar/categories/MenuList.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react"
 import classNames from "classnames"
-
+import {Link} from "gatsby"
 function MenuList({ item, onClickItem }) {
   const [activeItem, setActiveItem] = React.useState(null)
 
   const onSelectItem = index => {
     setActiveItem(index)
   }
-// {activeItem === index ? " active" : ""}
+
   return (
     <ul className="navbar-nav ">
       {item.map((name, index) => (
         <li
           onClick={() => onSelectItem(index)}
           key={`${name}_${index}`}
-          className="nav-item" 
+          className = "nav-item nav-link"
         >
-          <a className="nav-link " href="#">
+          <a className={activeItem === index ? "active " : ""} href="#" >
             {name}
           </a>
         </li>
