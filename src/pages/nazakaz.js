8a1063a76ca7a1e2a96e339fb0c9c62083ac6014
Alexei Pogorelov <alexeipogorelovbn@yandex.ru>
2020-09-04 18:38:36 +0300

    work

--- src/pages/nazakaz.js
+++ src/pages/nazakaz.js
@@ -1,7 +1,17 @@
 import React from "react"
+import Header from "../components/header/Header"
+import Container from "@material-ui/core/Container"
+import SwiperCake from "../components/swiper/SwiperCake"
+import Album from "../components/album/Album"
+
+import "../components/maincss/styles.scss"
+
 
 const Nazakaz = () => {
-  return <div>Заказ тортов</div>
+  return ( <Container maxWidth="md">
+  <Header />
+  <Album />
+</Container>)
 }
 
 export default Nazakaz
